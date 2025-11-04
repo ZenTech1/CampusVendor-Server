9bb00da3f72693d86d14090e0e3394c2d410d5dc
victor Pinto <vkpinto1234@gmail.com>
2025-11-04 16:45:24 +0000

    fixing stuff

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ import { swaggerUi, swaggerSpec } from "./config/swagger.js";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 10000 || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 //app.use(prisma);
